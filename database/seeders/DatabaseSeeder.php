e0e0561c5e2f39da367c0392aa8ed1432b37e743
Alan Ambrose <alanambrosedev@gmail.com>
2026-01-05 21:21:59 +0530

    Add Article, Category, and Tag models with factories and migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Article;
+use App\Models\Category;
+use App\Models\Tag;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -20,6 +23,40 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => '12345678',
         ]);
+
+        $tech = Category::factory()->create(['name' => 'Technology', 'slug' => 'technology']);
+        $life = Category::factory()->create(['name' => 'Lifestyle', 'slug' => 'lifestyle']);
+        $news = Category::factory()->create(['name' => 'News', 'slug' => 'news']);
+
+        $tags = Tag::factory(10)->create();
+
+        Article::factory(10)
+            ->for($tech)
+            ->create()
+            ->each(function ($article) use ($tags) {
+                $article->tags()->attach(
+                    $tags->random(rand(1, 3))->pluck('id')->toArray()
+                );
+            });
+
+        Article::factory(10)
+            ->for($news)
+            ->create()
+            ->each(function ($article) use ($tags) {
+                $article->tags()->attach(
+                    $tags->random(rand(1, 3))->pluck('id')->toArray()
+                );
+            });
+
+        Article::factory(10)
+            ->for($life)
+            ->create()
+            ->each(function ($article) use ($tags) {
+                $article->tags()->attach(
+                    $tags->random(rand(1, 3))->pluck('id')->toArray()
+                );
+            });
     }
 }
