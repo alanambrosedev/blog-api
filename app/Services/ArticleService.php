ce3dbfd518c65c618975b5514ce0739a9f0ec5de
Alan Ambrose <alanambrosedev@gmail.com>
2026-01-06 22:14:20 +0530

    Implement ArticleService with create, update, and delete functionalities

---
+++ app/Services/ArticleService.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Article;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+
+class ArticleService
+{
+    public function createArticle(array $data, ?UploadedFile $image = null)
+    {
+        return DB::transaction(function () use ($data, $image) {
+            $data['slug'] = Str::slug($data['title'].'-'.Str::random(6));
+
+            if ($image) {
+                $path = $image->store('articles', 'public');
+                $data['image'] = $path;
+            }
+
+            $article = Article::create($data);
+
+            if (isset($data['tags'])) {
+                $article->tags()->sync($data['tags']);
+            }
+
+            return $article;
+        });
+    }
+
+    public function updateArticle(Article $article, array $data, ?UploadedFile $image = null)
+    {
+        return DB::transaction(function () use ($article, $data, $image) {
+            if (isset($data['title'])) {
+                $data['slug'] = Str::slug($data['title']).'-'.Str::random(6);
+            }
+
+            if ($image) {
+                if ($article->image) {
+                    Storage::disk('public')->delete($article->image);
+                }
+                $data['image'] = $image->store('articles', 'public');
+            }
+
+            $article->update($data);
+            if (isset($data['tags'])) {
+                $article->tags()->sync($data['tags']);
+            }
+
+            return $article;
+        });
+    }
+
+    public function deleteArticle(Article $article)
+    {
+        if ($article->image) {
+            Storage::disk('public')->delete($article->image);
+        }
+        $article->delete();
+    }
+}
