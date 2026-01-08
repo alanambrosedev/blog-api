d5c4ef6f0965bfad88f34650db1acadb2aa612f0
Alan Ambrose <alanambrosedev@gmail.com>
2026-01-08 22:40:07 +0530

    Add CategoryService and TagService for category and tag management

---
+++ app/Services/CategoryService.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Category;
+use Illuminate\Support\Str;
+
+class CategoryService
+{
+    public function createCategory(array $data)
+    {
+        $data = Str::slug($data['name']);
+
+        return Category::create($data);
+    }
+
+    public function updateCategory(Category $category, array $data)
+    {
+        if (isset($data['name'])) {
+            $data['slug'] = Str::slug($data['name']);
+            $category->update($data);
+
+            return $category;
+        }
+    }
+
+    public function deleteCategory(Category $category)
+    {
+        $category->delete();
+    }
+}
---
+++ app/Services/TagService.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Tag;
+use Illuminate\Support\Str;
+
+class TagService
+{
+    public function createTag(array $data)
+    {
+        $data['slug'] = Str::slug($data['name']);
+
+        return Tag::create($data);
+    }
+
+    public function updateTag(Tag $tag, array $data)
+    {
+        if (isset($data['name'])) {
+            $data['slug'] = Str::slug($data['name']);
+            $tag->update($data);
+
+            return $tag;
+        }
+    }
+
+    public function deleteTag(Tag $tag)
+    {
+        $tag->delete();
+    }
+}
