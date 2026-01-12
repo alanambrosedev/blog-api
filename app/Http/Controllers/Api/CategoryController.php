f05db9914a47c5a862b61e36bc52508daaa01576
Alan Ambrose <alanambrosedev@gmail.com>
2026-01-12 19:48:38 +0530

    Implement CategoryController with CRUD operations and resource responses for category management

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -3,9 +3,37 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+use App\Http\Resources\CategoryResource;
 use App\Services\CategoryService;
 
 class CategoryController extends Controller
 {
     public function __construct(protected CategoryService $service) {}
+
+    public function index()
+    {
+        return CategoryResource::collection(Category::withCount('articles')->get());
+    }
+    public function store(StoreCategoryRequest $request)
+    {
+        $category = $this->service->createCategory($request->validated());
+        return new CategoryResource($category);
+    }
+    public function show(Category $category)
+    {
+        return new CategoryResource($category);
+    }
+    public function update(UpdateCategoryRequest $request, Category $category)
+    {
+        $updated = $this->service->updateCategory($category, $request->validated());
+        return new CategoryResource($updated);
+    }
+    public function destroy(Category $category)
+    {
+        $this->service->deleteCategory($category);
+        return response()->noContent();
+    }
 }
