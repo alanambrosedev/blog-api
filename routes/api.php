7b6d558f9233c4def21bdf1d5e392b2eff5cbc0b
Alan Ambrose <alanambrosedev@gmail.com>
2026-01-14 20:40:42 +0530

    Refactor API routes to use controller methods for user authentication and resource management; replace closure with AuthController for user retrieval.

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,23 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ArticleController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\TagController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/articles', [ArticleController::class, 'index']);
+Route::get('/articles/{article:slug}', [ArticleController::class, 'show']);
+Route::get('/categories', [CategoryController::class, 'index']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [AuthController::class, 'user']);
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    // Admin Resources
+    Route::apiResource('articles', ArticleController::class)->except(['index', 'show']);
+    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('tags', TagController::class);
+});
